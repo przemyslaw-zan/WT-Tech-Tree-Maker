52d3f9d0a40e505f3aa92bb542f709d52b8c1152
przemyslaw-zan <69513154+przemyslaw-zan@users.noreply.github.com>
2021-07-04 00:39:36 +0200

    Professional commit name

--- script.js
+++ script.js
@@ -2,6 +2,39 @@
 //remember about folders funcitonality
 //remember about ability to edit vehicles
 (() => {
+    Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.6.1/themes/classic/galleria.classic.min.js');
+    Galleria.run('.galleria')
+    Galleria.configure({
+        _toggleInfo: false
+    })
+
+    CKEDITOR.replace('vehicledescription')
+    CKEDITOR.replace('vehicledescriptionedit')
+    //CKEDITOR.config.uiColor = '#66AB16';
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = () => {
+        closeModal()
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            closeModal()
+        }
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+        $('.galleria').data('galleria').splice(0, $('.galleria').data('galleria').getDataLength());
+        $('.galleria').data('galleria').destroy()
+        Galleria.run('.galleria')
+    }
+
+
     let vehicleList = []
 
     const save = localStorage.getItem('save')
@@ -66,13 +99,132 @@
         }
     })
 
-    document.querySelector('#techtreetest').addEventListener('click', (e) => {
-        if (e.target.tagName !== 'LI') return
-        const vehicleId = e.target.id
-        const vehicle = vehicleList.find((item) => {
-            return item.id === vehicleId
+    document.querySelector('#vehicleimagelistadd').addEventListener('click',(e)=>{
+        e.preventDefault()
+        const list = document.querySelector('#vehicleimagelist')
+        list.appendChild(createImageListItem())
+    })
+
+    document.querySelector('#vehicleimagelisteditadd').addEventListener('click',(e)=>{
+        e.preventDefault()
+        const list = document.querySelector('#vehicleimagelistedit')
+        list.appendChild(createImageListItem())
+    })
+
+    function createImageListItem(url, description) {
+        let listItem = document.createElement('li')
+        let table = document.createElement('table')
+        let tbody = document.createElement('tbody')
+
+        let tr1 = document.createElement('tr')
+
+        let td1 = document.createElement('td')
+        td1.rowSpan = 2
+        let remove = document.createElement('div')
+        remove.style.cursor = 'pointer'
+        remove.innerText = '❌'
+        td1.appendChild(remove)
+        tr1.appendChild(td1)
+
+        remove.addEventListener('click',()=>{
+            listItem.remove()
         })
-        console.log(vehicle)
+
+        let td2 = document.createElement('td')
+        let moveUp = document.createElement('div')
+        moveUp.style.cursor = 'pointer'
+        moveUp.innerText = '⬆️'
+        td2.appendChild(moveUp)
+        tr1.appendChild(td2)
+
+        moveUp.addEventListener('click',()=>{
+            if (listItem.previousSibling) {
+                listItem.previousSibling.before(listItem)
+            }
+        })
+
+        let td3 = document.createElement('td')
+        let urlLabel = document.createElement('label')
+        urlLabel.innerText = 'URL:'
+        td3.appendChild(urlLabel)
+        tr1.appendChild(td3)
+
+        let td4 = document.createElement('td')
+        let urlInput = document.createElement('input')
+        if (url) urlInput.value = url
+        td4.appendChild(urlInput)
+        tr1.appendChild(td4)
+
+        let td5 = document.createElement('td')
+        td5.rowSpan = 2
+        td5.style.padding = 0
+        let img = document.createElement('img')
+        if (url) img.src = url
+        img.height = '50'
+        td5.appendChild(img)
+        tr1.appendChild(td5)
+
+        urlInput.addEventListener('change', (e)=>{
+            const url = e.target.value
+            img.src = url
+        })
+
+        let tr2 = document.createElement('tr')
+
+        let td6 = document.createElement('td')
+        let moveDown = document.createElement('div')
+        moveDown.style.cursor = 'pointer'
+        moveDown.innerText = '⬇️'
+        td6.appendChild(moveDown)
+        tr2.appendChild(td6)
+
+        moveDown.addEventListener('click',()=>{
+            if (listItem.nextSibling) {
+                listItem.nextSibling.after(listItem)
+            }
+        })
+
+        let td7 = document.createElement('td')
+        let descLabel = document.createElement('label')
+        descLabel.innerText = 'Caption:'
+        td7.appendChild(descLabel)
+        tr2.appendChild(td7)
+
+        let td8 = document.createElement('td')
+        let descInput = document.createElement('input')
+        if (description) descInput.value = description
+        td8.appendChild(descInput)
+        tr2.appendChild(td8)
+
+        tbody.appendChild(tr1)
+        tbody.appendChild(tr2)
+
+        table.appendChild(tbody)
+        listItem.appendChild(table)
+
+        return listItem
+    }
+
+    document.querySelector('#techtree').addEventListener('click', (e) => {
+        let element = e.target
+        if (!element.id) element = element.parentNode
+        if (!element.id) return
+        const vehicle = vehicleList.find((item) => {
+            return item.id === element.id
+        })
+        if (!vehicle.images&&!vehicle.description) return
+        document.querySelector('#modal_title').innerText = vehicle.name
+        if (vehicle.images) $('.galleria').data('galleria').load([...vehicle.images])
+        document.querySelector('.modal-body').innerHTML = vehicle.description
+        const info = document.querySelector('.galleria-info')
+        info.style.width = 'fit-content'
+        info.style.left = 'auto'
+        info.style.bottom = '50px'
+        info.style.top = 'auto'
+        info.style.right = '0px'
+        document.querySelector('.galleria-info-text').style.backgroundColor = 'RGBA(0, 0, 0, 0.75)'
+        document.querySelector('.galleria-info-text').style.padding = '3px'
+        modal.style.display = "block";
     })
 
     document.querySelector('#editionSelect').addEventListener('change', (e) => {
@@ -87,8 +239,16 @@
         document.querySelector('#vehicleconnectionedit').value = vehicle.connection
         document.querySelector('#vehiclebranchedit').value = vehicle.branch
         document.querySelector('#vehicleorderedit').value = vehicle.order
-        document.querySelector('#vehicledescriptionedit').value = vehicle.description
+        CKEDITOR.instances.vehicledescriptionedit.setData(vehicle.description)
         document.querySelector('#vehiclethumbnailedit').value = vehicle.thumbnail
+        if (vehicle.images) {
+            const list = document.querySelector('#vehicleimagelistedit')
+            for (let image of vehicle.images) {
+                const url = image.image?image.image:''
+                const desc = image.description?image.description:''
+                list.appendChild(createImageListItem(url,desc))
+            }
+        }
     })
 
     function readVehicleInput() {
@@ -103,9 +263,15 @@
         const connection = document.querySelector('#vehicleconnection').value
         const branch = Number(document.querySelector('#vehiclebranch').value)
         const order = Number(document.querySelector('#vehicleorder').value)
-        const description = document.querySelector('#vehicledescription').value
+        const description = CKEDITOR.instances.vehicledescription.getData()
         const id = 'v' + Date.now()
         const thumbnail = document.querySelector('#vehiclethumbnail').value
+        const images = []
+        document.querySelector('#vehicleimagelist').querySelectorAll('li').forEach((item)=>{
+            const image = item.querySelectorAll('input')[0].value
+            const description = item.querySelectorAll('input')[1].value.trim()
+            images.push({image,description})
+        })
         const vehicle = {
             name,
             rank,
@@ -116,7 +282,8 @@
             order,
             description,
             id,
-            thumbnail
+            thumbnail,
+            images
         }
         vehicleList.push(vehicle)
         document.querySelector('#newForm').reset()
@@ -134,9 +301,16 @@
         const connection = document.querySelector('#vehicleconnectionedit').value
         const branch = Number(document.querySelector('#vehiclebranchedit').value)
         const order = Number(document.querySelector('#vehicleorderedit').value)
-        const description = document.querySelector('#vehicledescriptionedit').value
+        const description = CKEDITOR.instances.vehicledescriptionedit.getData()
+        CKEDITOR.instances.vehicledescriptionedit.setData('')
         const id = document.querySelector('#editionSelect').value
         const thumbnail = document.querySelector('#vehiclethumbnailedit').value
+        const images = []
+        document.querySelector('#vehicleimagelistedit').querySelectorAll('li').forEach((item)=>{
+            const image = item.querySelectorAll('input')[0].value
+            const description = item.querySelectorAll('input')[1].value.trim()
+            images.push({image,description})
+        })
         const vehicle = {
             name,
             rank,
@@ -147,7 +321,8 @@
             order,
             description,
             id,
-            thumbnail
+            thumbnail,
+            images
         }
         vehicleList.forEach((element, index, array) => {
             if (element.id === vehicle.id) {
@@ -218,7 +393,7 @@
     }
 
     function drawTree(organizedVehicles) {
-        const tbody = document.querySelector('#techtreetest')
+        const tbody = document.querySelector('#techtree')
         tbody.innerHTML = ''
         const ranks = []
         const branches = []
@@ -233,11 +408,23 @@
         const techtree = document.querySelector('#techtree')
         techtree.innerHTML = ''
         for (let rank of ranks) {
+            let firstPremiumBranch = true
             let rankDiv = document.createElement('div')
             rankDiv.classList.add('rank')
+            let rankNumDiv = document.createElement('div')
+            rankNumDiv.classList.add('rankNumber')
+            let rankNumDivText = document.createElement('div')
+            rankNumDivText.innerHTML = `Rank <b>${romanize(rank)}</b>`
+            rankNumDivText.classList.add('rankNumberText')
+            rankNumDiv.appendChild(rankNumDivText)
+            rankDiv.appendChild(rankNumDiv)
             for (let branch of branches) {
                 let branchDiv = document.createElement('div')
                 branchDiv.classList.add('branch')
+                if (branch.indexOf('premium')!==-1&&firstPremiumBranch) {
+                    firstPremiumBranch = false
+                    branchDiv.style.borderLeft = '2px solid black'
+                }
                 let region = `rank_${rank}_branch_${branch}`
                 if (organizedVehicles[region] !== undefined) {
                     for (let vehicle of organizedVehicles[region]) {
@@ -279,7 +466,7 @@
                     <td rowspan="3" class="badgeSide"></td>
                 </tr>
                 <tr>
-                    <td class="vehicleBadge type_${vehicle.type} ${branchLine} connected_${vehicle.connection}" style="position:relative;">
+                    <td id="${vehicle.id}" class="vehicleBadge type_${vehicle.type} ${branchLine} connected_${vehicle.connection}" style="position:relative; ${vehicle.images||vehicle.description?'cursor:pointer;':''}">
                         <span class="vehicleName">${vehicle.name}</span>
                         <b class="vehicleBr">${vehicle.br.toFixed(1)}</b>
                         ${img}
@@ -428,4 +615,15 @@
             div.style.height = `${y2-y1}px`
         })
     }
+
+    function romanize(num) {
+        var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+        for ( i in lookup ) {
+          while ( num >= lookup[i] ) {
+            roman += i;
+            num -= lookup[i];
+          }
+        }
+        return roman;
+      }
 })()
