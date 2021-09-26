98a13830269db0b50e24a7cd593955d54b4116e6
przemyslaw-zan <69513154+przemyslaw-zan@users.noreply.github.com>
2021-09-26 02:49:06 +0200

    Updated description

--- template.js
+++ template.js
@@ -7,7 +7,7 @@ function createHtmlContent(title, description, tree, vehicles) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>${title}</title>
-        <meta name="description" content="${description}" />
+		<meta name="description" content="This tech tree was generated using WT-Tech-Tree-Maker. ${description}" />
         <meta name="generator" content="https://github.com/przemyslaw-zan/WT-Tech-Tree-Maker" />
         <link rel="icon" href="https://warthunder.com/i/favicons/mstile-144x144.png" />
     </head>
